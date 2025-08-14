0e59a7954ba6e65612326e1b9ec0784bb8cfb9dd
Genius30 <geniusporwal01@gmail.com>
2025-08-14 16:06:48 +0530

    Base theming and logos ready

---
+++ src/components/logos/rdtech-group-logo.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { SafeImage } from "../ui/safe-image";
+
+export default function RDTechGroupLogo({
+  size = 100,
+}: { readonly size?: number } = {}) {
+  return (
+    <div>
+      <SafeImage
+        src="/logos/rdtech-group-logo.png"
+        alt="Logo"
+        width={size}
+        height={size}
+      />
+    </div>
+  );
+}
