b616dd2e983498d46e8bf2b722db78579ad4c243
Mohd Zaid <[razvizaid259@gmail.com]>
2025-08-15 16:04:23 +0530

    adds page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import About from '@/components/about'
+import Footer from '@/components/footer'
+
+function page() {
+  return (
+    <div>
+      <About />
+      <Footer />
+    </div>
+  )
+}
+
+export default page
